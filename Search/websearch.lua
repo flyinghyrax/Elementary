c5168ba06b64d6dc67f2219c8e90ced379cea5b9
Nicolas <hombro.coder@gmail.com>
2017-12-23 16:06:27 -0500

    url encode the query

---
+++ Search/websearch.lua
@@ -0,0 +1,10 @@
+function url_encode(str)
+  if (str) then
+    str = string.gsub (str, "\n", "\r\n")
+    str = string.gsub (str, "([^%w %-%_%.%~])",
+        function (c) return string.format ("%%%02X", string.byte(c)) end)
+    str = string.gsub (str, " ", "+")
+  end
+  print("Returning " .. str)
+  return str
+end
